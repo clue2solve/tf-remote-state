de9ff93e0cec663c3af8cfc1283a027cce2a98f4
Anand Rao <anand@honnu.com>
2020-06-22 23:55:28 -0700

    updated readme

--- provider.tf
+++ provider.tf
@@ -4,13 +4,5 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "rao-tf-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-west-2"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "tf-state-locks"
-    encrypt        = true
-  }
+
 }
